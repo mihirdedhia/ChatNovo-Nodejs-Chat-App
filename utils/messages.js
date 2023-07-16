8c02da0fb3211527bdcad9f09f7f7ccb42b10039
Mihir Dedhia <78468690+mihirdedhia@users.noreply.github.com>
2023-07-16 21:09:48 +0530

    added IST time zone

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,14 @@
 const moment = require('moment');
 
 function formatMessage(username, text) {
+    
+    // to show time in Indian Standard Time (IST) since India has been on time offset of UTC+5:30
+    // console.log(moment().utcOffset("+05:30").format('h:mm a'));
+    
     return {
         username,
         text,
-        time: moment().format('h:mm a'),
+        time: moment().utcOffset("+05:30").format('h:mm a')
     };
 }
 
